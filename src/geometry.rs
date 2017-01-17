e2544b64f5f05c27023b4e3f03b83f8f6a962cdf
Michael Bohn <michael.bohn@freiheit.com>
2017-01-17 06:54:41 +0100

    cross- and dotProduct impl && parallel test in intersection

--- src/geometry.rs
+++ src/geometry.rs
@@ -10,16 +10,25 @@ struct Triangle {
     v2: Vec3f
 }
 
+const EPSILON:f32 = 1e-8;
+
 impl Object for Triangle {
     fn intersect(self, orig: &Vec3f, direction: &Vec3f ) -> bool {
 
+        // TODO optimisation: compute the normal once!
         // compute plane's normal
         let v0v1 = self.v1 - &self.v0;
         let v0v2 = self.v2 - &self.v0;
+        let normal = v0v1.crossProduct(&v0v2);
 
-        //Vec3f N = v0v1.crossProduct(v0v2); // N
-        //float denom = N.dotProduct(N);
+        // check if parallel
+        let planeDirAngle = normal.dotProduct(direction);
+        if planeDirAngle.abs() < EPSILON {
+            //approx. parallel, on intersection
+            return false;
+        }
 
+        // TODO http://www.scratchapixel.com/code.php?id=9&origin=/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle
 
         return false;
     }
--- src/vector.rs
+++ src/vector.rs
@@ -16,14 +16,26 @@ impl Vec3f {
     pub fn new_i32(x: i32, y: i32, z: i32) -> Vec3f {
         return Vec3f{x: (x as f32), y: (y as f32), z: (z as f32)};
     }
+
+    pub fn crossProduct(self, other: &Vec3f) -> Vec3f {
+        return Vec3f::new(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x);
+    }
+
+    pub fn dotProduct(self, other: &Vec3f) -> f32 {
+        return self.x * other.x +
+               self.y * other.y +
+               self.z * other.z;
+    }
 }
 
 impl<'a> Sub<&'a Vec3f> for Vec3f {
     type Output = Vec3f;
 
     fn sub(self, other: &Vec3f) -> Vec3f {
-        // TODO really implement vector sub
-        return Vec3f::new_i32(0,0,0);
+        return Vec3f::new(self.x - other.x, self.y - other.y, self.z - other.z)
     }
 }
 
@@ -34,7 +46,17 @@ mod tests {
 
     #[test]
     fn test_sub() {
-        // TODO real test
-        assert_eq!(Vec3f::new_i32(1,0,0), Vec3f::new_i32(1,1,1) - &Vec3f::new_i32(2,2,2));
+        assert_eq!(Vec3f::new_i32(1,1,1), Vec3f::new_i32(2,2,2) - &Vec3f::new_i32(1,1,1));
+        assert_eq!(Vec3f::new(-3.04, -4.04, -5.04), Vec3f::new(-4.14, -5.14, -6.14) - &Vec3f::new(-1.1, -1.1, -1.1));
+    }
+
+    #[test]
+    fn test_crossProduct() {
+        assert_eq!(Vec3f::new_i32(0,-1,0), Vec3f::new_i32(1,0,0).crossProduct(&Vec3f::new_i32(0,0,1)));
+    }
+
+    #[test]
+    fn test_dotProduct() {
+        assert_eq!(12.0, Vec3f::new_i32(1,2,3).dotProduct(&Vec3f::new_i32(4, -5, 6)));
     }
 }
