dec9f01768e079eb07757ea316b2f40b2ab8e04f
Michael Bohn <michael.bohn@freiheit.com>
2017-01-19 06:51:21 +0100

    ray position on triange plane

--- src/geometry.rs
+++ src/geometry.rs
@@ -19,14 +19,25 @@ impl Object for Triangle {
         // compute plane's normal
         let v0v1 = self.v1 - &self.v0;
         let v0v2 = self.v2 - &self.v0;
-        let normal = v0v1.crossProduct(&v0v2);
+        let normal = v0v1.cross_product(&v0v2);
 
         // check if parallel
-        let planeDirAngle = normal.dotProduct(direction);
-        if planeDirAngle.abs() < EPSILON {
+        let plane_dir_angle = normal.dot_product(direction);
+        if plane_dir_angle.abs() < EPSILON {
             //approx. parallel, on intersection
             return false;
         }
+        let d = normal.dot_product(&self.v0);
+        let t = (normal.dot_product(direction) + d) / plane_dir_angle;
+
+        if t.is_sign_negative() { // check if behind the ray
+            return false
+        }
+
+        // point on triangle plane
+        let p = orig + &(direction * t);
+
+        // TODO inside out test!
 
         // TODO http://www.scratchapixel.com/code.php?id=9&origin=/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle
 
--- src/vector.rs
+++ src/vector.rs
@@ -1,5 +1,5 @@
 
-use std::ops::Sub;
+use std::ops::{Sub, Add, Mul};
 
 #[derive(Debug, PartialEq)]
 pub struct Vec3f {
@@ -17,25 +17,41 @@ impl Vec3f {
         return Vec3f{x: (x as f32), y: (y as f32), z: (z as f32)};
     }
 
-    pub fn crossProduct(self, other: &Vec3f) -> Vec3f {
+    pub fn cross_product(&self, other: &Vec3f) -> Vec3f {
         return Vec3f::new(
             self.y * other.z - self.z * other.y,
             self.z * other.x - self.x * other.z,
             self.x * other.y - self.y * other.x);
     }
 
-    pub fn dotProduct(self, other: &Vec3f) -> f32 {
+    pub fn dot_product(&self, other: &Vec3f) -> f32 {
         return self.x * other.x +
                self.y * other.y +
                self.z * other.z;
     }
 }
 
-impl<'a> Sub<&'a Vec3f> for Vec3f {
+impl<'a, 'b> Sub<&'b Vec3f> for Vec3f {
     type Output = Vec3f;
 
     fn sub(self, other: &Vec3f) -> Vec3f {
-        return Vec3f::new(self.x - other.x, self.y - other.y, self.z - other.z)
+        return Vec3f::new(self.x - other.x, self.y - other.y, self.z - other.z);
+    }
+}
+
+impl<'a,'b> Add<&'b Vec3f> for &'a Vec3f {
+    type Output = Vec3f;
+
+    fn add(self, other: &Vec3f) -> Vec3f {
+        return Vec3f::new(self.x + other.x, self.y + other.y, self.z + other.z);
+    }
+}
+
+impl<'a> Mul<f32> for &'a Vec3f {
+    type Output = Vec3f;
+
+    fn mul(self, s: f32) -> Vec3f {
+        return Vec3f::new(self.x * s, self.y * s, self.z * s);
     }
 }
 
@@ -51,12 +67,24 @@ mod tests {
     }
 
     #[test]
-    fn test_crossProduct() {
-        assert_eq!(Vec3f::new_i32(0,-1,0), Vec3f::new_i32(1,0,0).crossProduct(&Vec3f::new_i32(0,0,1)));
+    fn test_add() {
+        assert_eq!(Vec3f::new_i32(3,4,5), &Vec3f::new_i32(2,3,4) + &Vec3f::new_i32(1,1,1));
+        assert_eq!(Vec3f::new(-5.24, -6.24, -7.24), &Vec3f::new(-4.14, -5.14, -6.14) + &Vec3f::new(-1.1, -1.1, -1.1));
     }
 
     #[test]
-    fn test_dotProduct() {
-        assert_eq!(12.0, Vec3f::new_i32(1,2,3).dotProduct(&Vec3f::new_i32(4, -5, 6)));
+    fn test_mul() {
+        assert_eq!(&Vec3f::new(-3.14, 4.14, 5.24) * 2.0, Vec3f::new(-6.28, 8.28, 10.48));
+        assert_eq!(&Vec3f::new_i32(1, 1, 1) * -1.0, Vec3f::new_i32(-1, -1, -1));
+    }
+
+    #[test]
+    fn test_cross_product() {
+        assert_eq!(Vec3f::new_i32(0,-1,0), Vec3f::new_i32(1,0,0).cross_product(&Vec3f::new_i32(0,0,1)));
+    }
+
+    #[test]
+    fn test_dot_product() {
+        assert_eq!(12.0, Vec3f::new_i32(1,2,3).dot_product(&Vec3f::new_i32(4, -5, 6)));
     }
 }
