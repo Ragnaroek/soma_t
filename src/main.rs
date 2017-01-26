d1f173c85467267f09a3e2fef6b937a1a4aa5d1d
Michael Bohn <michael.bohn@freiheit.com>
2017-01-26 06:48:23 +0100

    print frame buffer to console (xterm)

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,33 @@ use std::io::Write;
 use vector::Vec3f;
 use geometry::{Object, Triangle};
 
-fn out_demo() {
-    print!("\x1B[?1049h"); //enter alternate screen
+const FRAME_WIDTH : usize = 220;//120;
+const FRAME_HEIGHT: usize = 53;//30;
+const FOV: f32 = 51.52;
+
+fn draw_buffer(buffer: &[[bool; FRAME_HEIGHT]; FRAME_WIDTH]) {
+
+    print!("\x1B[?1049h"); //enter alternate screen mode
     print!("\x1B[H");
-    print!("alternate screen mode");
-    print!("\x1B\x5B9B"); //move cursor 9 lines down
+
+    for y in 0..FRAME_HEIGHT {
+        for x in 0..FRAME_WIDTH {
+            if buffer[x][y] {
+                print!("\u{2588}");
+            }
+            print!("\x1B\x5B1C"); // move right one column (1C)
+        }
+        print!("\x1B\x5B1B"); //move down one row (1B)
+        print!("\x1B\x5B{}D", FRAME_WIDTH);
+    }
 
     // TODO Exit alternate screen to restore terminal
 
-    io::stdout().flush();
-
+    io::stdout().flush().ok().expect("Could not flush stdout");;
     let mut input = String::new();
     let result = io::stdin().read_line(&mut input);
     println!("{:?}", result);
 }
-
-const FRAME_WIDTH : usize = 120;
-const FRAME_HEIGHT: usize = 30;
-
-const FOV: f32 = 51.52;
 
 fn main() {
 
@@ -47,17 +55,15 @@ fn main() {
     for x in 0..FRAME_WIDTH {
         for y in 0..FRAME_HEIGHT {
 
-            let x_screen = 2.0 * ((x as f32) + 0.5) / ((FRAME_WIDTH as f32) - 1.0) * aspect_ratio * fov_scale;
-            let y_screen = (1.0 - 2.0 * ((y as f32) + 0.5)) / (FRAME_HEIGHT as f32) * fov_scale;
+            let x_screen = (2.0 * ((x as f32) + 0.5) / (FRAME_WIDTH as f32) - 1.0) * aspect_ratio * fov_scale;
+            let y_screen = ((1.0 - 2.0 * ((y as f32) + 0.5)) / (FRAME_HEIGHT as f32)) * fov_scale;
             let dir = Vec3f::new(x_screen, y_screen, -1.0).normalize();
 
             if tri.intersect(&orig, &dir) {
-                println!("ray intersection {:?},{:?}", x, y);
                 frame_buffer[x][y] = true;
             }
         }
     }
 
-
-    // TODO draw frame_buffer to console
+    draw_buffer(&frame_buffer);
 }
