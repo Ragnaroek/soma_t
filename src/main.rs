adcae168047c0ed584e171befb9c7c9458423667
Michael Bohn <michael.bohn@freiheit.com>
2017-02-02 06:06:01 +0100

    fixed rendering bugs

--- src/main.rs
+++ src/main.rs
@@ -73,16 +73,16 @@ fn main() {
     let mut frame_buffer = [[false; FRAME_HEIGHT]; FRAME_WIDTH];
 
     //fov scale
-    let fov_scale =  FOV.to_radians().tan();
-    let aspect_ratio = (FRAME_WIDTH / FRAME_HEIGHT) as f32;
-
+    let fov_scale =  (FOV * 0.5).to_radians().tan();
+    let aspect_ratio = (FRAME_WIDTH as f32) / (FRAME_HEIGHT as f32);
     let orig = Vec3f::new_i32(0, 0, 0);
 
     for x in 0..FRAME_WIDTH {
         for y in 0..FRAME_HEIGHT {
 
             let x_screen = (2.0 * ((x as f32) + 0.5) / (FRAME_WIDTH as f32) - 1.0) * aspect_ratio * fov_scale;
-            let y_screen = ((1.0 - 2.0 * ((y as f32) + 0.5)) / (FRAME_HEIGHT as f32)) * fov_scale;
+            let y_screen = (1.0 - 2.0 * ((y as f32) + 0.5) / (FRAME_HEIGHT as f32)) * fov_scale;
+
             let dir = Vec3f::new(x_screen, y_screen, -1.0).normalize();
 
             if tri.intersect(&orig, &dir) {
